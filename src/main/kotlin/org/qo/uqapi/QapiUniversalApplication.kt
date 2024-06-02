eb3248c2c864181dc8c0892f33d82a383e363b48
glowingstone124 <hanserofficial@outlook.com>
2024-06-02 13:20:29 +0800

    chat component

--- src/main/kotlin/org/qo/uqapi/QapiUniversalApplication.kt
+++ src/main/kotlin/org/qo/uqapi/QapiUniversalApplication.kt
@@ -2,7 +2,6 @@ package org.qo.uqapi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import java.util.concurrent.ScheduledExecutorService
 
 @SpringBootApplication
 class QapiUniversalApplication
